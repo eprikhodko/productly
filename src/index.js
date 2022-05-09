6faa20305a8dfa8d221dd3e1452029f426524a16
eprikhodko <eprikhodkodev@gmail.com>
2022-05-09 22:29:28 +0300

    feat: make tags hide and show strategies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './scss/main.scss';
 
 const STRATEGIES_TAGS_CONTAINER = document.querySelector('.strategies__tags');
 const TAGS = document.querySelectorAll('.strategies__tags .tag');
+const STRATEGIES = document.querySelectorAll('.strategy-wrapper .strategy');
 
 // по загрузке страницы? вызовем функцию addTagsClickHandler()
 window.onload = function () {
@@ -19,6 +20,13 @@ const addTagsClickHandler = () => {
       removeSelectedTags();
       // добавим оранжевый цвет тегу, по которому мы кликнули
       selectClickedTag(clickedTag);
+
+      if (clickedTag.innerText === 'All') {
+        showAllStrategies();
+      } else {
+        // передаем в фильтр содержимое тега по которому мы кликнули
+        filterStrategyBySelectedTag(clickedTag.innerText);
+      }
     }
   });
 };
@@ -33,4 +41,25 @@ const removeSelectedTags = () => {
 const selectClickedTag = (tag) => {
   tag.classList.add('tag_selected');
   tag.classList.remove('tag_bordered');
+};
+
+const showAllStrategies = () => {
+  STRATEGIES.forEach((strategy) => {
+    strategy.classList.remove('strategy_hidden');
+  });
+};
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+  // будем перебирать все стратегии
+  STRATEGIES.forEach((strategy) => {
+    // сначала скроем эту стратегию
+    strategy.classList.add('strategy_hidden');
+    // выберем все теги этой стратегии, и будем проверять, равен ли какой то тег внутри стратегии, тегу, на который мы кликнули
+    const strategyTags = strategy.querySelectorAll('.tag');
+    strategyTags.forEach((tag) => {
+      if (tag.innerText === selectedTag) {
+        strategy.classList.remove('strategy_hidden');
+      }
+    });
+  });
 };
